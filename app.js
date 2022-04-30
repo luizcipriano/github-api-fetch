f741abe023298c7292ca1729779a6f97af2e9f82
Luiz Cipriano <77516067+luizcipriano@users.noreply.github.com>
2022-04-30 01:35:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const box = document.querySelector('.box_user');
+
+async function puxarDados() {
+  try {
+    const name = document.querySelector('.name').value;
+    const dadosResponse = await fetch(`https://api.github.com/users/${name}`);
+    const dadosJSON = await dadosResponse.json();
+    const img = dadosJSON.avatar_url;
+    const user = dadosJSON.name;
+    const loc = dadosJSON.location;
+    const bio = dadosJSON.bio;
+    box.innerHTML = `<img src="${img}" alt=""> <p class="user">${user}</p> <p class="user">${bio}</p>  <p class="user">${loc}</p>`
+    
+    console.log(dadosJSON);
+    // document.body.innerText = dadosJSON.titulo;
+  }
+  catch(erro) {
+    console.log(erro);
+  }
+}
